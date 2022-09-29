38e49f76e52f714226980a16fae8de9cd0622391
DJ-MrJay <97578860+DJ-MrJay@users.noreply.github.com>
2022-09-29 22:23:10 +0300

    Add components

--- src/components/Calculator-class-based.js
+++ src/components/Calculator-class-based.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import '../scss/Calculator.scss';
-import style from '../scss/Style.module.scss';
 import calculate from '../logic/calculate';
 
 const buttons = [
@@ -121,61 +120,61 @@ const buttons = [
   },
 ];
 
-const Calculator = () => {
-  const [prevState, setState] = useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
 
-  const handleClick = (e) => {
-    const display = () => calculate(prevState, e.target.innerHTML);
-    setState(display);
+  handleClick = (e) => {
+    this.setState((prevState) => calculate(prevState, e.target.innerText));
   };
 
-  let result;
-  const { total, next, operation } = prevState;
-  if (next === null && operation === null && total === null) {
-    result = 0;
-  }
+  render() {
+    const { total, next, operation } = this.state;
+    let result;
+    if (next === null && operation === null && total === null) {
+      result = 0;
+    }
 
-  if (next !== null && total === null) {
-    result = next;
-  }
+    if (next !== null && operation === null && total === null) {
+      result = next;
+    }
 
-  if (next === null && operation !== null && total !== null) {
-    result = total + operation;
-  }
+    if (next === null && operation !== null && total !== null) {
+      result = total + operation;
+    }
 
-  if (next !== null && operation !== null && total !== null) {
-    result = total + operation + next;
-  }
+    if (next !== null && operation !== null && total !== null) {
+      result = total + operation + next;
+    }
 
-  if (next === null && operation === null && total !== null) {
-    result = total;
-  }
+    if (next === null && operation === null && total !== null) {
+      result = total;
+    }
 
-  return (
-    <>
-      <h1 className={style.calcTitle}>Let&apos;s do some math!</h1>
+    return (
       <div className="Calculator">
         <span className="Display">{result}</span>
         <section className="Keypad">
           {buttons.map((button) => (
             <button
               type="button"
-              onClick={handleClick}
+              onClick={this.handleClick}
               key={button.id}
               className={`Button ${button.name} ${button.color}`}
-              value={button.symbol}
             >
               {button.symbol}
             </button>
           ))}
         </section>
       </div>
-    </>
-  );
-};
+    );
+  }
+}
 
 export default Calculator;
